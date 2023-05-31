bdf27f9da08c45e7ea6d5eb85c47dd2cf089dc04
Nabil El Bajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-31 14:34:35 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['next'],
+  rules: {
+    // ...
+  },
+};
